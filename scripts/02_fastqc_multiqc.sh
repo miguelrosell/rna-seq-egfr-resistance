963f296ad69eafcc563c02470f12effe1aadb0e7
miguelrosell <miguelnorty@gmail.com>
2025-09-15 19:53:55 +0200

    Update 02_fastqc_multiqc.sh

--- scripts/02_fastqc_multiqc.sh
+++ scripts/02_fastqc_multiqc.sh
@@ -1,3 +1,7 @@
+## Once we have the fastq.gz files, we perform quality control on them using these commands. 
+# The qc_reports/ directory will contain the HTML files with the quality plots. 
+#FastQC works directly with .fastq.gz files and there is no need to decompress them.
+
 #!/bin/bash
 set -euo pipefail
 PROJECT_ROOT="$HOME/Documentos/Bioinfo_projects/rna-seq-egfr"
